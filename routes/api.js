044609ea29ae35b0db70cb68e10585c9dbf3a23f
Andrew <ACodding@mba2025.hbs.edu>
2025-01-03 19:42:39 -0500

    Replace bcrypt with bcryptjs

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const multer = require('multer');
 const stream = require('stream');
 const authenticateToken = require("../middlewares/auth");
